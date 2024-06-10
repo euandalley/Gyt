0b155f4cece00e269591080bb4e7c8cd94bd0398
Euan Dalley <euandalley@gmail.com>
2024-06-11 02:00:33 +0400

    Implement hash-object

--- cli.py
+++ cli.py
@@ -11,7 +11,9 @@ def main() -> None:
 
 
 def parse_args() -> argparse.Namespace:
-    """Parser for commands"""
+    """
+    Parser for commands
+    """
     parser = argparse.ArgumentParser()
 
     commands = parser.add_subparsers(dest='command')
@@ -20,9 +22,23 @@ def parse_args() -> argparse.Namespace:
     initParser = commands.add_parser('init')
     initParser.set_defaults(func=cmd_init)
 
+    hashObjParser = commands.add_parser('hash-object')
+    hashObjParser.set_defaults(func=cmd_hash_object)
+    hashObjParser.add_argument('file')
+
     return parser.parse_args()
 
 
 def cmd_init(args):
-    """Creates a new empty repository"""
+    """
+    Creates a new empty repository with init command
+    """
     data.init()
+
+
+def cmd_hash_object(args):
+    """
+    Implement the hash-object command
+    """
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
--- data.py
+++ data.py
@@ -1,5 +1,19 @@
 import os
+import hashlib
+
+GIT_DIR = '.gyt'
+
+def init() -> None:
+    os.makedirs(GIT_DIR)
 
 
-def init():
-    os.makedirs('.git')
+def hash_object(data:bytes) -> str:
+    """
+    Hash contents of file, store file contents under the hash
+    """
+    oid = hashlib.sha1(data).hexdigest()
+
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as f:
+        f.write(data)
+
+    return oid
